7fe3201e111de42ecdf9cb127df0dcd355e1ffa8
akshay <66055053+akshay-A09@users.noreply.github.com>
2024-04-02 12:12:58 +0530

    about

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us | Trust Group</title>
+    <title>About | Trust Group</title>
     <?php include 'head.php'; ?>
 </head>
 <body>
