4c92d13e8e7695e34816e910f7e2facac5ce9e89
Gaurav Patil <gaurav.agency09@gmail.com>
2024-04-02 16:00:24 +0530

    plustus page created

---
+++ plutus.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Plutus | Trust Group</title>
+    <?php include 'head.php'; ?>
+</head>
+<body>
+<?php include 'header.php'; ?>
+
+
+<div class="subHeader_min">
+<div class="subHeader">
+  <div class="container">
+  <div class="subHeader_wrapper">
+  <div class="subHeader_left">
+    <span>Home</span> / <span>Plutus</span> 
+  </div>
+  <div class="subHeader_nav">
+    <a href="#">Overview</a>
+    <a href="#aboutSection">Investment Banking</a>
+    <a href="#Capital_Market_Services">Capital Market Services</a>
+    <a href="#Products">Products</a>
+    <a href="#GetInTouch">Contact</a>
+  </div>
+
+
+  </div>
+  </div>
+</div></div>
+
+
+
+
+<main>
+
+
+
+
+<!-- About -->
+<section class="aboutMain backBG_work" id="aboutSection">
+
+<div class="backBG_work_img">
+  <img src="assets/images/debt.jpg" style="object-position: bottom left;">
+</div>
+
+<div class="container pr">
+
+<div class="about_wrapper df g-20">
+<div class="about_wrapper_left  w-50 m-0">
+    <h5 class="heading_top tu textGold">Wealth  Management</h5>
+    <img class="mt-20" src="assets/images/trust-plutus.png">
+    <h2 class="heading mt-20">A Transparent Approach to Wealth  Management</h2>
+    <p>TrustPlutus is the Wealth Management arm of TRUST, an independent and trusted Wealth Management Firm that focuses on your investment needs and the best way to meet those needs. We provide you with an open architecture platform for investing. We have a fiduciary responsibility to act in your best interest. A client-centric platform offering bespoke wealth solutions.</p>
+
+  
+    <ul class="aboutUl countList">
+    <li>
+            <h2><span class="count">14800</span>Cr +</h2>
+            <p>Assets under our care</p>
+        </li>
+        <li>
+            <h2><span class="count">80</span>+</h2>
+            <p>Our team strength</p>
+        </li>
+        <li>
+            <h2><span class="count">655</span> +</h2>
+            <p>Families and individuals</p>
+        </li>
+
+    </ul>
+    
+
+    
+</div>
+
+</div></div></section>
+<!-- About End -->
+
+<section class="assetsAllocation"><div class="container">
+<h2 class="heading textBlack">Your Asset Allocation</h2>
+
+<div class="assets-list">
+<ul>
+<li><div><span>01</span></div><h3>You -  centric</h3></li>
+
+<li><div><span>02</span></div><h3>Understand  your goals</h3></li>
+
+<li><div><span>03</span></div><h3>Your portfolio</h3></li>
+</ul>
+</div>
+
+</div></section>
+
+<!-- What We Offer -->
+<section class="whatWeOffer"><div class="container">
+<h5 class="heading_top tu textGold">What We Offer</h5>
+<h2 class="heading textBlack">Multi-family office range of services</h2>
+<div class="whatWeOffer_row">
+
+<div class="whatWeOffer_left">
+  <div class="tab-container research_tabs">
+    <div class="tab active" data-tab="ibcs-tab">
+      <h3>Advisory services</h3>
+   
+    </div>
+    <div class="tab" data-tab="mbd-tab">
+      <h3>Wealth management services</h3>
+    </div>
+    <div class="tab" data-tab="structuring-tab">
+      <h3>Legacy services</h3>
+    </div>
+  </div>
+</div>
+
+<div class="whatWeOffer_right" style="background-image: url(assets/images/what_we_offer.jpg);">
+
+<div id="ibcs-tab" class="tab-content active"><div class="tab-content_row">
+<ul>
+        <li>Wealth management & Investment Advisory</li>
+        <li>Proprietary Wealth Management Solutions</li>
+        <li>Taxation</li>
+        <li>Compliance</li>  
+      </ul>
+</div></div>
+
+<div id="mbd-tab" class="tab-content"><div class="tab-content_row">
+<h3>Merchant  Banking (MBD) </h3>  
+<p>We are a Category I Merchant Banker, providing services such as underwriting securities, advising on corporate restructuring, mergers and acquisitions, and portfolio management services, among others.</p>
+</div></div>
+
+<div id="structuring-tab" class="tab-content"><div class="tab-content_row">
+<h3>Structuring</h3>  
+<p>Our structuring team acts as the catalyst for innovative debt instruments, working hand-in-hand with clients, traders, salespeople, and analysts to unlock your unique financial goals.</p>
+</div></div>
+
+</div>
+ 
+</div>
+
+</div></section>
+<!-- What We Offer -->
+
+
+<!-- Why TRUST -->
+<section class="whyTRUST center"><div class="container">
+<h5 class="heading_top tu textGold">How We Work</h5>
+<h2 class="heading">Lorem Ipsum</h2>
+
+<div class="whyTRUST_wrapper"><ul>
+<li>
+<div class="whyTRUST_img"><img src="assets/images/work_with_us/flexibility.jpg" alt="Why TRUST"></div>
+<div class="whyTRUST_data">
+<h3>Risk Profiling</h3>
+<p>Determine your risk profile and your short/long term goals</p>  
+</div>
+</li>
+
+<li>
+<div class="whyTRUST_img"><img src="assets/images/work_with_us/growth.jpg" alt="Why TRUST"></div>
+<div class="whyTRUST_data">
+<h3>Asset Allocation</h3>
+<p>Portfolio diversification in line with your risk profile</p>  
+</div>
+</li>
+
+<li>
+<div class="whyTRUST_img"><img src="assets/images/work_with_us/diverse.jpg" alt="Why TRUST"></div>
+<div class="whyTRUST_data">
+<h3>Investment Solutions</h3>
+<p>Products and services that are aligned with your asset allocation</p>  
+</div>
+</li>
+
+<li>
+<div class="whyTRUST_img"><img src="assets/images/work_with_us/innovative.jpg" alt="Why TRUST"></div>
+<div class="whyTRUST_data">
+<h3>Portfolio Review</h3>
+<p>Get a complete picture and periodic review of your investments</p>  
+</div>
+</li>
+</ul></div>
+
+
+</div></section>
+<!-- Why TRUST End -->
+
+
+
+<!-- Get in Touch -->
+<section class="hafel" id="GetInTouch"><div class="container">
+
+<div class="hafelRow">
+  <div class="hafelCol_left m-auto">
+    <h2 class="heading textGold">Get in Touch</h2>
+  </div>
+  <div class="hafelCol_right m-0">
+  <div class="contactMinUlRow">
+
+  <div class="contactMinUl df">
+    <div class="contactMinUlCol">
+      <h3><i class="fa-solid fa-phone"></i> Phone:</h3>
+      <p><a href="#">+91 22 6274 6000</a></p>
+
+      <h3><i class="fa-solid fa-envelope"></i> E-mail:</h3>
+      <p><a href="#">info@trustplutus.com</a></p>
+    </div>
+
+    <div class="contactMinUlCol">
+    <h3><i class="fa-solid fa-location-dot"></i> Corporate Office:</h3>
+      <p>801, Naman Centre,<br>
+        Bandra Kurla Complex,<br>
+        Bandra (East),<br>
+        Mumbai - 400 051</p>
+      <div class="mt-20">
+      <a href="#applyNow_form" class="btn093 btnGold"><span>View in Map <i class="fa-solid fa-arrow-right arrowR" aria-hidden="true"></i></span></a>
+      </div>
+    </div>
+  </div>
+
+  <hr class="hrGold line">
+
+<div class="contactMinUlBottom center">
+  <h3>For more information, visit <a href="https://trustsecuirties.in" target="_blank" class="textGold">trustsecuirties.in</a></h3>
+</div>
+
+</div>
+
+  </div>
+</div>
+
+</div></section>
+<!-- Get in Touch End -->
+
+
+
+
+
+</main>
+<?php include 'footer.php'; ?>
+
+<!-- Tabs -->
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+  var tabs = document.querySelectorAll('.tab');
+  tabs.forEach(function(tab) {
+    tab.addEventListener('click', function() {
+      var tabId = this.getAttribute('data-tab');
+      activateTab(tabId);
+      history.pushState(null, null, '#' + tabId);
+    });
+  });
+  var hash = window.location.hash.substr(1);
+  if (hash !== "") {
+    activateTab(hash);
+  }
+  function activateTab(tabId) {
+    var tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach(function(content) {
+      content.classList.remove('active');
+    });
+    document.getElementById(tabId).classList.add('active');
+
+    tabs.forEach(function(t) {
+      t.classList.remove('active');
+      if (t.getAttribute('data-tab') === tabId) {
+        t.classList.add('active');
+      }
+    });
+  }
+});
+
+
+// counting effect
+function animateCount(element) {
+        var endValue = parseInt(element.textContent);
+        var duration = 2000;
+        var interval = 10;
+        var step = (endValue / duration) * interval;
+        var current = 0;
+
+        var timer = setInterval(function () {
+            current += step;
+            if (current >= endValue) {
+                clearInterval(timer);
+                element.textContent = endValue;
+            } else {
+                element.textContent = Math.round(current);
+            }
+        }, interval);
+    }
+
+    function startCounting(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                observer.unobserve(entry.target);
+                var counts = entry.target.getElementsByClassName('count');
+                for (var i = 0; i < counts.length; i++) {
+                    animateCount(counts[i]);
+                }
+            }
+        });
+    }
+
+    var options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.6
+    };
+
+    var observer = new IntersectionObserver(startCounting, options);
+    var target = document.getElementById('aboutSection');
+    observer.observe(target);
+// counting effect End
+
+</script>
+<!-- Tabs End-->
+
+
+
+</body>
+</html>
